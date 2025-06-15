c5daee4735a72db04f9864d678bca2ad7c422084
Jason Zhu <703632667@qq.com>
2025-06-15 11:29:22 +0800

    add riscv64 toolchain
    Signed-off-by: Jason Zhu <703632667@qq.com>
    Change-Id: I1182380761edc12a497efc57e023e7d049a7e090

--- make-riscv64.sh
+++ make-riscv64.sh
@@ -1,1 +1,1 @@
-make CROSS_COMPILE=/home/adminpc/code/rk/prebuilts/gcc/linux-x86/riscv64/riscv64-lp64d--glibc--stable-2024.05-1/bin/riscv64-buildroot-linux-gnu- PLATFORM=generic
+make CROSS_COMPILE=riscv64-linux-gnu- PLATFORM=generic
